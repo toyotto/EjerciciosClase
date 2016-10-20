7d416f29a5762f9e3b9cda6df86f66a919344743
cesar velasco <cesar velasco@cesarvelasco>
2016-10-20 18:18:32 -0500

    Ejercicio laboratorio 1

---
+++ Personal/src/personal/Personal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personal;
+
+/**
+ *
+ * @author cesar velasco
+ */
+public class Personal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.util.Scanner leer = new java.util.Scanner(System.in);
+        String name;
+        name = leer.next();
+        NewJFrame=name;
+        // TODO code application logic here
+    }
+    
+}
